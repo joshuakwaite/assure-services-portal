7ed6e207a0cf373fcc90a4f9a8a6ed240644e522
Josh Waite <kalewaite@gmail.com>
2017-05-11 16:00:50 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { NewDealComponent } from './new-deal/new-deal.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'new-deal', component: NewDealComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'my-account', component: MyAccountComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  // { path: '**', component: `PageNotFoundComponent` }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    DocumentsComponent,
+    NewDealComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    MyAccountComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-deal/new-deal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-deal',
+  templateUrl: './new-deal.component.html',
+  styleUrls: ['./new-deal.component.css']
+})
+export class NewDealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
